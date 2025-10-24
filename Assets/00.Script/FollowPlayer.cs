b93f2b609ece7036ff0f4b6b063d8a5787366448
KHKAC <rudgnsrb888@naver.com>
2025-10-24 15:31:14 +0900

    251024 [SerializeField] 추가

--- Assets/00.Script/FollowPlayer.cs
+++ Assets/00.Script/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] GameObject player;
-    Vector3 offset = new Vector3(0, 5, -12);
+    [SerializeField] Vector3 offset = new Vector3(0, 5, -12);
 
     // 카메라 이동은 LateUpdate에서 실행할 수 있도록
     void LateUpdate()
